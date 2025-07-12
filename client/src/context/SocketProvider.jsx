8e1a9184edcb325639d6a40f207d58de66de40bd
Adarsh Gupta <adarsh.signin173@gmail.com>
2025-07-12 23:37:33 +0530

    Env variable backend url

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -1,18 +1,22 @@
 import React, { createContext, useMemo, useContext } from "react";
-import { io } from 'socket.io-client'
-const SocketContext = createContext(null)
+import { io } from "socket.io-client";
+
+const SocketContext = createContext(null);
 
 export const useSocket = () => {
-  const socket = useContext(SocketContext)
-  return socket
-}
+  const socket = useContext(SocketContext);
+  return socket;
+};
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:8000"), []);
+  const socket = useMemo(() => {
+    const URL = process.env.REACT_APP_SOCKET_URL || "http://localhost:8000";
+    return io(URL);
+  }, []);
 
   return (
     <SocketContext.Provider value={socket}>
       {props.children}
     </SocketContext.Provider>
-  )
-}
+  );
+};
